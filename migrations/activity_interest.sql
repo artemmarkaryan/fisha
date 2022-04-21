53a3497c7f6aa106e7c055864c892090edb08d47
artemmarkaryan <artemmarkaryan@yandex.ru>
2022-04-22 00:18:51 +0300

    activity_interest fix

--- migrations/activity_interest.sql
+++ migrations/activity_interest.sql
@@ -1,3 +1,5 @@
+drop table if exists "activity_interest";
+
 create table if not exists "activity_interest"
 (
     activity_id     int    not null,
@@ -7,6 +9,7 @@ create table if not exists "activity_interest"
     constraint activity_interest__interest_id_fk foreign key (interest_id) references "interest" (id) on delete cascade on update cascade,
 
     rank        float4 not null default 0,
+    constraint activity_interest__rank_boundaries check ( rank >= -1 and rank <= 1 ),
 
     created_at  timestamp       default current_timestamp,
     updated_at  timestamp       default current_timestamp

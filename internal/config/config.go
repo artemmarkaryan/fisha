1e1d99f3701b38602f07a01847d8aad577ebc3f9
artemmarkaryan <artemmarkaryan@yandex.ru>
2022-04-22 02:01:00 +0300

    set env

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,9 @@
 package config
+
+const (
+	DatabaseHostKey     = "DATABASE_HOST"
+	DatabasePortKey     = "DATABASE_PORT"
+	DatabaseUserKey     = "DATABASE_USER"
+	DatabasePasswordKey = "DATABASE_PASSWORD"
+	DatabaseDBNameKey   = "DATABASE_DBNAME"
+)
